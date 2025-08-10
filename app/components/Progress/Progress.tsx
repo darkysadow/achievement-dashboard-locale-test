7562f564d0d2c80e1b88ca0fc78b838b86ea8624
darkysadow <oleksandr.poberezhniak@gmail.com>
2025-08-10 14:49:57 +0300

    Added stats

--- app/components/Progress/Progress.tsx
+++ app/components/Progress/Progress.tsx
@@ -5,25 +5,37 @@ import styles from './Progress.module.scss'
 
 type LinearProgressBarProps = {
   progress: number
+  variant: 'primary' | 'secondary'
 }
 
-export default function LinearProgressBar({ progress = 0 }: LinearProgressBarProps) {
+export default function LinearProgressBar({
+  progress = 0,
+  variant = 'primary',
+}: LinearProgressBarProps) {
   const [displayProgress] = useState(progress)
 
   return (
-    <div className={styles.progressBar}>
+    <div className={`${styles.progressBar} ${styles[`progressBar-${variant}`]}`}>
       <div
         className={styles.progress}
         style={{
-          width: `${displayProgress}%`,
+          width:
+            variant === 'secondary'
+              ? `calc(${Math.min(Math.max(displayProgress, 0), 100)}% - 2px)`
+              : `${Math.min(Math.max(displayProgress, 0), 100)}%`,
         }}
       />
       <div
         className={styles.emptyProgress}
         style={{
-          width: `${100 - displayProgress}%`,
+          width:
+            variant === 'secondary'
+              ? `calc(${100 - Math.min(Math.max(displayProgress, 0), 100)}% - 2px)`
+              : `${100 - Math.min(Math.max(displayProgress, 0), 100)}%`,
+          display: displayProgress >= 100 ? 'none' : 'block',
+          marginLeft: variant === 'secondary' ? '4px' : '0', // Додаємо відступ для gap
         }}
-      ></div>
+      />
     </div>
   )
 }
--- app/components/TotalItems/TotalItems.tsx
+++ app/components/TotalItems/TotalItems.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from './TotalItems.module.scss'
 import { AllAchievements } from '@/app/types/types'
+import { AchievementsIcon } from '../icons/icons'
+import LinearProgressBar from '../Progress/Progress'
 
 interface TotalItemsProps {
   achievements: AllAchievements
@@ -14,6 +16,50 @@ const TotalItems = (props: TotalItemsProps) => {
         <div className={styles.titleBlock}>
           <h1>{achievements.title}</h1>
           <p>{achievements.bestText[0] + ' ' + achievements.percent + achievements.bestText[1]}</p>
+        </div>
+        <div className={styles.mainInfo}>
+          <div className={styles.stats}>
+            <div className={styles.progressContainer}>
+              <div className={styles.progressTitleContainer}>
+                <div className={styles.progressTitle}>
+                  <div className={styles.progressTitleIcon}>
+                    <AchievementsIcon />
+                  </div>
+                  <h2>{achievements.progress.text}</h2>
+                </div>
+                <div className={styles.progressTotal}>
+                  <span>{achievements.progress.value}</span> <p>/ {achievements.progress.total}</p>
+                </div>
+              </div>
+              <div className={styles.progressbarContainer}>
+                <LinearProgressBar
+                  variant="secondary"
+                  progress={Math.min(
+                    (achievements.progress.value / achievements.progress.total) * 100,
+                    100,
+                  )}
+                />
+              </div>
+            </div>
+            <div className={styles.achievementsRanks}>
+              <div className={styles.achievementsRank}>
+                <p>{achievements.firstRank.value}</p>
+                <h3>{achievements.firstRank.text}</h3>
+              </div>
+              <div className={styles.achievementsRank}>
+                <p>{achievements.secondRank.value}</p>
+                <h3>{achievements.secondRank.text}</h3>
+              </div>
+              <div className={styles.achievementsRank}>
+                <p>{achievements.thirdRank.value}</p>
+                <h3>{achievements.thirdRank.text}</h3>
+              </div>
+              <div className={styles.achievementsRank}>
+                <p>{achievements.closed.value}</p>
+                <h3>{achievements.closed.text}</h3>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
